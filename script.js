59f19601adaedd74bc2bc56e1ec9be15fed88642
Natalie Acevedo <natalieacevedo25@gmail.com>
2021-09-29 22:42:53 +0100

    finally was able to find the mistake of right button

--- script.js
+++ script.js
@@ -1,10 +1,45 @@
 const slides = document.querySelectorAll('.slide');
 
-slides.forEach((s, i) => {
+const slider = document.querySelector('.slider');
 
-    s.style.transform = `translateX(${100 * i}%`);
+const bottomLeft = document.querySelector('.slider__btn--left');
 
-    console.log(s);
+const bottomRight = document.querySelector('.slider__btn--right');
 
+let maxSlide = slides.length -1;
+
+let currentSlide = 0;
+
+
+
+console.log(slides);
+
+
+console.log(bottomLeft);
+
+console.log(bottomRight);
+
+
+slider.style.transform = `scale(0.3) translatex(-300px)`;
+
+
+
+bottomRight.addEventListener('click', function () {
+
+    if (currentSlide === maxSlide) {
+        currentSlide = 0;
+    } else {
+
+        currentSlide++;
+    }
+
+    slides.forEach((s, i) => {
+
+        
+        console.log(currentSlide);
+        s.style.transform = `translatex(${100 * (i - currentSlide)}%)`;
+        
+    });
+    
 });
 
