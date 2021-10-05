3d8c5df6d5361d264573315e4d4535c78685ecee
SusanaAlvarezZuluaga <susialvarezzuluaga@hotmail.com>
2021-10-06 00:10:02 +0100

    added the food page

--- scriptSusana.js
+++ scriptSusana.js
@@ -1,42 +1,4 @@
 
-const placesToVisit = [
-    {
-      name: "Tokyo",
-      type: "city",
-      sliderPicture: "https://lp-cms-production.imgix.net/2021-04/Pontocho_Kyoto.jpg?auto=format&fit=crop&sharp=10&vib=20&ixlib=react-8.6.4&w=850&q=50&dpr=2",
-      popUpPicture: "https://lp-cms-production.imgix.net/2021-04/Pontocho_Kyoto.jpg?auto=format&fit=crop&sharp=10&vib=20&ixlib=react-8.6.4&w=850&q=50&dpr=2",
-      description: "this is a place bla bla bla",
-    },
-    {
-      name: "Naoshima",
-      type: "island",
-      sliderPicture: "https://www.erikastravelventures.com/wp-content/uploads/2019/08/P1010422-1024x762.jpg",
-      popUpPicture:"https://www.erikastravelventures.com/wp-content/uploads/2019/08/P1010422-1024x762.jpg",
-      description: "this is a place bla bla bla",
-    },
-    {
-      name: "Memorial Cenotaph ",
-      type: "type",
-      sliderPicture: "https://lp-cms-production.imgix.net/2021-04/shutterstockRF_1393700846.jpg?auto=format&fit=crop&sharp=10&vib=20&ixlib=react-8.6.4&w=850&q=50&dpr=2",
-      popUpPicture:"https://lp-cms-production.imgix.net/2021-04/shutterstockRF_1393700846.jpg?auto=format&fit=crop&sharp=10&vib=20&ixlib=react-8.6.4&w=850&q=50&dpr=2",
-      description: "this is a place bla bla bla",
-    },
-    {
-      name: "Yakushima",
-      type: "island",
-      sliderPicture: "https://lp-cms-production.imgix.net/2021-04/shutterstockRF_423304240.jpg?auto=format&fit=crop&sharp=10&vib=20&ixlib=react-8.6.4&w=850&q=50&dpr=2",
-      popUpPicture:"https://lp-cms-production.imgix.net/2021-04/shutterstockRF_423304240.jpg?auto=format&fit=crop&sharp=10&vib=20&ixlib=react-8.6.4&w=850&q=50&dpr=2",
-      description: "this is a place bla bla bla",
-    },
-    {
-      name: "Kamikōchi",
-      type: "type",
-      sliderPicture: "https://lp-cms-production.imgix.net/2021-04/shutterstockRF_1254380497.jpg?auto=format&fit=crop&sharp=10&vib=20&ixlib=react-8.6.4&w=850&q=50&dpr=2",
-      popUpPicture:"https://lp-cms-production.imgix.net/2021-04/shutterstockRF_1254380497.jpg?auto=format&fit=crop&sharp=10&vib=20&ixlib=react-8.6.4&w=850&q=50&dpr=2",
-      description: "this is a place bla bla bla",
-    }
-];
-
 //popup stuff
 function showPopUp(name) {
   var popUpToShow = document.getElementById(name);
@@ -137,40 +99,99 @@ function createCard(name,type,sliderPicture,popUpPicture,description){
       hidePopUp(name)
     });
 }
+//PLACES PAGE
 
-for(let i=0;i<placesToVisit.length;i++){
+const placesToVisit = [
+  {
+    name: "Tokyo",
+    type: "city",
+    sliderPicture: "https://lp-cms-production.imgix.net/2021-04/Pontocho_Kyoto.jpg?auto=format&fit=crop&sharp=10&vib=20&ixlib=react-8.6.4&w=850&q=50&dpr=2",
+    popUpPicture: "https://lp-cms-production.imgix.net/2021-04/Pontocho_Kyoto.jpg?auto=format&fit=crop&sharp=10&vib=20&ixlib=react-8.6.4&w=850&q=50&dpr=2",
+    description: "this is a place bla bla bla",
+  },
+  {
+    name: "Naoshima",
+    type: "island",
+    sliderPicture: "https://www.erikastravelventures.com/wp-content/uploads/2019/08/P1010422-1024x762.jpg",
+    popUpPicture:"https://www.erikastravelventures.com/wp-content/uploads/2019/08/P1010422-1024x762.jpg",
+    description: "this is a place bla bla bla",
+  },
+  {
+    name: "Memorial Cenotaph ",
+    type: "type",
+    sliderPicture: "https://lp-cms-production.imgix.net/2021-04/shutterstockRF_1393700846.jpg?auto=format&fit=crop&sharp=10&vib=20&ixlib=react-8.6.4&w=850&q=50&dpr=2",
+    popUpPicture:"https://lp-cms-production.imgix.net/2021-04/shutterstockRF_1393700846.jpg?auto=format&fit=crop&sharp=10&vib=20&ixlib=react-8.6.4&w=850&q=50&dpr=2",
+    description: "this is a place bla bla bla",
+  },
+  {
+    name: "Yakushima",
+    type: "island",
+    sliderPicture: "https://lp-cms-production.imgix.net/2021-04/shutterstockRF_423304240.jpg?auto=format&fit=crop&sharp=10&vib=20&ixlib=react-8.6.4&w=850&q=50&dpr=2",
+    popUpPicture:"https://lp-cms-production.imgix.net/2021-04/shutterstockRF_423304240.jpg?auto=format&fit=crop&sharp=10&vib=20&ixlib=react-8.6.4&w=850&q=50&dpr=2",
+    description: "this is a place bla bla bla",
+  },
+  {
+    name: "Kamikōchi",
+    type: "type",
+    sliderPicture: "https://lp-cms-production.imgix.net/2021-04/shutterstockRF_1254380497.jpg?auto=format&fit=crop&sharp=10&vib=20&ixlib=react-8.6.4&w=850&q=50&dpr=2",
+    popUpPicture:"https://lp-cms-production.imgix.net/2021-04/shutterstockRF_1254380497.jpg?auto=format&fit=crop&sharp=10&vib=20&ixlib=react-8.6.4&w=850&q=50&dpr=2",
+    description: "this is a place bla bla bla",
+  }
+];
+
+function displayPlaces(){
+  for(let i=0;i<placesToVisit.length;i++){
     createCard(placesToVisit[i].name,placesToVisit[i].type,placesToVisit[i].sliderPicture,placesToVisit[i].popUpPicture,placesToVisit[i].description);
+  }
 }
 
-//carrousel thing
-$(".carousel").owlCarousel({
-    margin: 20,
-    loop: true,
-    autoplay: false,
-    responsiveClass:true,
-    responsive: {
-      0:{
-        items:1,
-        nav: false,
-        stagePadding:50,
-      },
-      400:{
-        items:1,
-        nav: false,
-        stagePadding:100,
-      },
-      1000:{
-        items:1,
-        nav: false,
-        stagePadding:150,
-      },
-      1500:{
-        items:1,
-        nav: false,
-        stagePadding:200,
-      }
-    }
-});
+//food to eat page
+const foodToEat = [
+  {
+    name: "Sushi",
+    type: "raw",
+    sliderPicture:"https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/0749D9BC-260D-40F4-A07F-54814C4A82B4/Derivates/A73A7793-F3EE-4B90-ABA4-1CC1A0C3E18F.jpg",
+    popUpPicture:"https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/0749D9BC-260D-40F4-A07F-54814C4A82B4/Derivates/A73A7793-F3EE-4B90-ABA4-1CC1A0C3E18F.jpg",
+    description: "Sushi is a traditional Japanese dish of prepared vinegared rice (鮨飯, sushi-meshi), usually with some sugar and salt, accompanied by a variety of ingredients (ネタ, neta), such as seafood, often raw, and vegetables. Styles of sushi and its presentation vary widely, but the one key ingredient is 'sushi rice', also referred to as shari or sumeshi",
+  },
+  {
+  
+  name: "Ramen",
+  type: "Soup",
+  sliderPicture:"https://www.thewholeworldisaplayground.com/wp-content/uploads/2016/12/Japan-Must-Eats-Best-Food-3.webp",
+  popUpPicture:"https://www.thewholeworldisaplayground.com/wp-content/uploads/2016/12/Japan-Must-Eats-Best-Food-3.webp",
+  description: "this is a place bla bla bla",
+},
+
+{
+  name: "Tempura",
+  type: "fried",
+  sliderPicture: "https://resources.matcha-jp.com/archive_files/jp/2015/09/ginza_gonpachi_20140914v.jpg",
+  popUpPicture:"https://resources.matcha-jp.com/archive_files/jp/2015/09/ginza_gonpachi_20140914v.jpg",
+  description: "this is a  food bla bla bla",
+},
+
+{
+  name: "Shabu-shabu",
+  type: "hotpot",
+  sliderPicture: "https://www.thewholeworldisaplayground.com/wp-content/uploads/2016/12/Japan-Must-Eats-Best-Food-31.webp",
+  popUpPicture:"https://www.thewholeworldisaplayground.com/wp-content/uploads/2016/12/Japan-Must-Eats-Best-Food-31.webp",
+  description: "this is a place bla bla bla",
+},
+{
+  name: "Tofu",
+  type: "vegetarian",
+  sliderPicture: "https://images.japancentre.com/page_elements/image1s/1515/original/tofu-bean-curd.jpg?1470240775",
+  popUpPicture:"https://images.japancentre.com/page_elements/image1s/1515/original/tofu-bean-curd.jpg?1470240775",
+  description: "this is a place bla bla bla",
+}
+];
+function displayFood(){
+  for(let i=0;i<foodToEat.length;i++){
+    createCard(foodToEat[i].name,foodToEat[i].type,foodToEat[i].sliderPicture,foodToEat[i].popUpPicture,foodToEat[i].description);
+  }
+}
+
 
 
 
